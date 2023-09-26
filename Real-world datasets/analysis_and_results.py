29c76122f15e37d0816fabd4deb98c4dda6bc31c
KalmanNet <87475986+KalmanNet@users.noreply.github.com>
2023-09-26 17:55:00 +0300

    Update analysis_and_results.py

--- Real-world datasets/analysis_and_results.py
+++ Real-world datasets/analysis_and_results.py
@@ -21,19 +21,3 @@ def plot_MSE(data_y, data_GT, x_hat, time, P_MSE_dB, P_MSE_m, index_vec, vehicul
     plt.savefig(vehicular+'/results/position_filters_'+direc+'_hor.pdf', bbox_inches='tight')
     plt.show()
 
-  ############################################################
-    def int2str(time):
-        return str(np.round(time, 2))
-
-    table = [[ "",                 "RMSE[m]",    " MSE[dB]",                               ],
-          ['Noise floor', int2str(P_MSE_m[5, int(index_vec[5, 0]), int(index_vec[5, 1])]),  int2str(P_MSE_dB[5, int(index_vec[5, 0]),int(index_vec[5, 1])])],
-          ['KF',          int2str(P_MSE_m[0, int(index_vec[0, 0]), int(index_vec[0, 1])]),  int2str(P_MSE_dB[0, int(index_vec[0, 0]),int(index_vec[0, 1])])],
-          ['ORKF',        int2str(P_MSE_m[1, int(index_vec[1, 0]), int(index_vec[1, 1])]),  int2str(P_MSE_dB[1, int(index_vec[1, 0]),int(index_vec[1, 1])])],
-          ['Chi-squared', int2str(P_MSE_m[2, int(index_vec[2, 0]), int(index_vec[2, 1])]),  int2str(P_MSE_dB[2, int(index_vec[2, 0]),int(index_vec[2, 1])])],
-          ['OIKF-AM',     int2str(P_MSE_m[3, int(index_vec[3, 0]), int(index_vec[3, 1])]),  int2str(P_MSE_dB[3, int(index_vec[3, 0]),int(index_vec[3, 1])])],
-          ['OIKF-EM',     int2str(P_MSE_m[4, int(index_vec[4, 0]), int(index_vec[4, 1])]),  int2str(P_MSE_dB[4, int(index_vec[4, 0]),int(index_vec[4, 1])]),]]
-
-    print(tabulate(table, headers='firstrow', tablefmt='fancy_grid', numalign='center'))
-    with open(vehicular+'/results/Tab_MSE_'+direc+'.txt', 'w') as f:
-        f.write(tabulate(table, headers='firstrow', tablefmt='latex', numalign='center'))
-    f.close()
